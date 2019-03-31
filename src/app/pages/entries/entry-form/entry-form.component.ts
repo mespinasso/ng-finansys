4cd73d966e918e9e40c7855c08d65b8442df5a77
mespinasso <matheus.espinasso@gmail.com>
2019-03-31 14:43:14 -0300

    Adding EntryFormComponent

---
+++ src/app/pages/entries/entry-form/entry-form.component.ts
@@ -0,0 +1,168 @@
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Entry } from '../shared/entry.model';
+import { EntryService } from '../shared/entry.service';
+
+import { switchMap } from 'rxjs/operators';
+
+import toastr from 'toastr';
+
+@Component({
+  selector: 'app-entry-form',
+  templateUrl: './entry-form.component.html',
+  styleUrls: ['./entry-form.component.scss']
+})
+export class EntryFormComponent implements OnInit, AfterContentChecked {
+
+  currentAction: string;
+  entryForm: FormGroup;
+  pageTitle: string;
+  serverErrorMessages: string[] = null;
+  submittingForm: boolean = false;
+  entry: Entry = new Entry();
+
+  constructor(
+    private entryService: EntryService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit() {
+    this.setCurrentAction();
+    this.buildEntryForm();
+    this.loadEntry();
+  }
+
+  ngAfterContentChecked() {
+    this.setPageTitle();
+  }
+
+  /**
+   * Form submission event
+   */
+  submitForm() {
+    this.submittingForm = true;
+
+    if (this.currentAction == 'new') {
+      this.createEntry();
+    } else {
+      this.updateEntry();
+    }
+  }
+
+  // PRIVATE METHODS
+
+  /**
+   * Sets current action [NEW/EDIT]
+   */
+  private setCurrentAction() {
+    if (this.route.snapshot.url[0].path == 'new')
+      this.currentAction = 'new';
+    else
+      this.currentAction = 'edit';
+  }
+
+  /**
+   * Sets entry form
+   */
+  private buildEntryForm() {
+    this.entryForm = this.formBuilder.group({
+      id: [null],
+      name: [null, [Validators.required, Validators.minLength(2)]],
+      description: [null],
+      type: [null, [Validators.required]],
+      amount: [null, [Validators.required]],
+      date: [null, [Validators.required]],
+      paid: [null, [Validators.required]],
+      categoryId: [null, [Validators.required]]
+    });
+  }
+
+  /**
+   * Loads entry if editing
+   */
+  private loadEntry() {
+    if (this.currentAction == 'edit') {
+      this.route.paramMap.pipe(
+        switchMap(params => this.entryService.getById(+params.get('id')))
+      )
+        .subscribe(
+          (entry) => {
+            this.entry = entry;
+            this.entryForm.patchValue(this.entry);
+          },
+          () => alert('An error occurred while fetching the entry')
+        )
+    }
+  }
+
+  /**
+   * Sets the page title
+   */
+  private setPageTitle() {
+    if (this.currentAction == 'new') {
+      this.pageTitle = 'New Entry';
+    } else {
+      const entryName = this.entry.name || '';
+      this.pageTitle = `Editing Entry: ${entryName}`;
+    }
+  }
+
+  /**
+   * Creates entry with form data
+   */
+  private createEntry() {
+    const entry: Entry = Object.assign(new Entry(), this.entryForm.value);
+
+    this.entryService.create(entry)
+      .subscribe(
+        entry => this.actionsForSuccess(entry),
+        error => this.actionsForError(error)
+      )
+  }
+
+  /**
+   * Updates entry with form data
+   */
+  private updateEntry() {
+    const entry: Entry = Object.assign(new Entry(), this.entryForm.value);
+
+    this.entryService.update(entry)
+      .subscribe(
+        entry => this.actionsForSuccess(entry),
+        error => this.actionsForError(error)
+      )
+  }
+
+  /**
+   * Shows success message and reloads form
+   * @param entry Entry
+   */
+  private actionsForSuccess(entry: Entry) {
+    toastr.success('Successfully processed!');
+
+    this.router.navigateByUrl("entries", {skipLocationChange: true}).then(
+      () => this.router.navigate(['entries', entry.id, 'edit'])
+    )
+  }
+
+  /**
+   * Shoes error message
+   * @param error Error
+   */
+  private actionsForError(error) {
+    toastr.error('An error occurred!');
+    console.error(error);
+
+    this.submittingForm = false;
+
+    if (error.status === 422) {
+      this.serverErrorMessages = JSON.parse(error._body).errors;
+    } else {
+      this.serverErrorMessages = ['Server error'];
+    }
+  }
+}
