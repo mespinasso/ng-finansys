8556180d2a76d5c27d8c4d4a99158052d6a9ddd6
mespinasso <matheus.espinasso@gmail.com>
2019-05-01 22:14:04 -0300

    Adding BaseResourceModel

--- src/app/pages/entries/shared/entry.model.ts
+++ src/app/pages/entries/shared/entry.model.ts
@@ -1,6 +1,7 @@
-import {Category} from '../../categories/shared/category.model';
+import { BaseResourceModel } from '../../../shared/models/base-resource.model';
+import { Category } from '../../categories/shared/category.model';
 
-export class Entry {
+export class Entry extends BaseResourceModel {
   constructor(
     public id?: number,
     public name?: string,
@@ -10,8 +11,10 @@ export class Entry {
     public date?: string,
     public paid?: boolean,
     public categoryId?: number,
-    public category?: Category
-  ) { }
+    public category?: Category) {
+
+    super();
+  }
 
   static types = {
     expense: 'Expense',
