a92002d0d3a1efdda63e64328ce6808885dee7ef
mespinasso <matheus.espinasso@gmail.com>
2019-05-03 23:39:06 -0300

    Adding BaseResourceFormComponent

---
+++ src/app/shared/components/base-resource-form/base-resource-form.component.ts
@@ -0,0 +1,171 @@
+import { OnInit, AfterContentChecked, Injector } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { BaseResourceModel } from '../../models/base-resource.model';
+import { BaseResourceService } from '../../services/base-resource.service';
+
+import {switchMap } from 'rxjs/operators';
+
+import toastr from 'toastr';
+
+export abstract class BaseResourceFormComponent<T extends BaseResourceModel> implements OnInit, AfterContentChecked {
+
+  currentAction: string;
+  resourceForm: FormGroup;
+  pageTitle: string;
+  serverErrorMessages: string[] = null;
+  submittingForm: boolean = false;
+
+  protected route: ActivatedRoute;
+  protected router: Router;
+  protected formBuilder: FormBuilder;
+
+  constructor(
+    protected injector: Injector,
+    public resource: T,
+    protected resourceService: BaseResourceService<T>,
+    protected jsonDataToResourceFn: (jsonData) => T) {
+
+    this.route = this.injector.get(ActivatedRoute);
+    this.router = this.injector.get(Router);
+    this.formBuilder = this.injector.get(FormBuilder);
+  }
+
+  ngOnInit() {
+    this.setCurrentAction();
+    this.buildResourceForm();
+    this.loadResource();
+  }
+
+  ngAfterContentChecked() {
+    this.setPageTitle();
+  }
+
+  /**
+   * Form submission event
+   */
+  submitForm() {
+    this.submittingForm = true;
+
+    if (this.currentAction == 'new') {
+      this.createResource();
+    } else {
+      this.updateResource();
+    }
+  }
+
+  // PRIVATE METHODS
+
+  /**
+   * Sets current action [NEW/EDIT]
+   */
+  protected setCurrentAction() {
+    if (this.route.snapshot.url[0].path == 'new')
+      this.currentAction = 'new';
+    else
+      this.currentAction = 'edit';
+  }
+
+  /**
+   * Loads resource if editing
+   */
+  protected loadResource() {
+    if (this.currentAction == 'edit') {
+      this.route.paramMap.pipe(
+        switchMap(params => this.resourceService.getById(+params.get('id')))
+      )
+        .subscribe(
+          (resource) => {
+            this.resource = resource;
+            this.resourceForm.patchValue(this.resource);
+          },
+          () => alert('An error occurred while fetching the category')
+        )
+    }
+  }
+
+  /**
+   * Sets the page title
+   */
+  protected setPageTitle() {
+    if (this.currentAction == 'new') {
+      this.pageTitle = this.creationPageTitle();
+    } else {
+      this.pageTitle = this.editionPageTitle();
+    }
+  }
+
+  /**
+   * Creation page title
+   */
+  protected creationPageTitle() : string {
+    return 'New';
+  }
+
+  /**
+   * Edition page title
+   */
+  protected editionPageTitle() : string {
+    return 'New';
+  }
+
+  /**
+   * Creates resource with form data
+   */
+  protected createResource() {
+    const resource: T = this.jsonDataToResourceFn(this.resourceForm.value);
+
+    this.resourceService.create(resource)
+      .subscribe(
+        resource => this.actionsForSuccess(resource),
+        error => this.actionsForError(error)
+      )
+  }
+
+  /**
+   * Updates resource with form data
+   */
+  protected updateResource() {
+    const category: T = this.jsonDataToResourceFn(this.resourceForm.value);
+
+    this.resourceService.update(category)
+      .subscribe(
+        resource => this.actionsForSuccess(resource),
+        error => this.actionsForError(error)
+      )
+  }
+
+  /**
+   * Shows success message and reloads form
+   * @param resource Resource
+   */
+  protected actionsForSuccess(resource: T) {
+    toastr.success('Successfully processed!');
+
+    const baseComponentPath : string = this.route.snapshot.parent.url[0].path;
+
+    this.router.navigateByUrl(baseComponentPath, {skipLocationChange: true}).then(
+      () => this.router.navigate([baseComponentPath, resource.id, 'edit'])
+    )
+  }
+
+  /**
+   * Shoes error message
+   * @param error Error
+   */
+  protected actionsForError(error) {
+    toastr.error('An error occurred!');
+    console.error(error);
+
+    this.submittingForm = false;
+
+    if (error.status === 422) {
+      this.serverErrorMessages = JSON.parse(error._body).errors;
+    } else {
+      this.serverErrorMessages = ['Server error'];
+    }
+  }
+
+  protected abstract buildResourceForm() : void;
+}
