faaa9bb8a95baba46a4c4e4b0e3260e8166450cb
mespinasso <matheus.espinasso@gmail.com>
2019-05-05 23:11:16 -0300

    Refactoring CategoryListComponent

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { BaseResourceListComponent } from '../../../shared/components/base-resource-list/base-resource-list.component';
 
 import { Category } from '../shared/category.model';
 import { CategoryService } from '../shared/category.service';
@@ -8,27 +10,9 @@ import { CategoryService } from '../shared/category.service';
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.scss']
 })
-export class CategoryListComponent implements OnInit {
+export class CategoryListComponent extends BaseResourceListComponent<Category> {
 
-  categories: Category[] = [];
-
-  constructor(private categoryService: CategoryService) { }
-
-  ngOnInit() {
-    this.categoryService.getAll().subscribe(
-      categories => this.categories = categories,
-      () => alert('Error while fetching the categories list')
-    )
-  }
-
-  deleteCategory(category: Category) {
-    const mustDelete = confirm(`Are you sure you want to delete the "${category.name}" category?`);
-
-    if (mustDelete) {
-      this.categoryService.delete(category.id).subscribe(
-        () => this.categories = this.categories.filter(element => element != category),
-        () => alert('Error while deleting')
-      )
-    }
+  constructor(private categoryService: CategoryService) {
+    super(categoryService);
   }
 }
